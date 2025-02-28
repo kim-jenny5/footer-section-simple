b31c6893b128829570e76f65820370d459438cc6
Jenny Kim <kimj1905@gmail.com>
2025-02-28 18:02:27 -0500

    push

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+<footer class='footer-container'>
+	<div></div>
+	<div></div>
+	<div>©️ {new Date().getFullYear}</div>
+</footer>;
